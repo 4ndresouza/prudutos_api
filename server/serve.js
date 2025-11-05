234a80c2ab64da36bb8cf0b866422a4fdee7f1a6
ANDRE AUGUSTO CESAR QUEIROZ DE SOUZA <andresouza6178@digitalcollege.internal>
2025-11-04 22:04:18 -0300

    subindo o segundo teste api

--- server/serve.js
+++ server/serve.js
@@ -1,4 +1,7 @@
 import pg from 'pg';
+import express from 'express';
+
+const app = express();
 const { Pool, Client } =pg;
 
 const pool = new Pool ({
@@ -24,3 +27,33 @@ console.log(`Erro ao conectar ao banco: ${error}`);
 /* fazer um select para testar a conexâo */
 const result = await pool.query('SELECT * FROM  produtos');
 console.log(`Resultado ${result}`);
+
+
+/* criando api com express */
+
+app.get('/',(req,res) => {
+res.send ('API de Produtos funcionando!' ) 
+});
+
+app.get('/produtos', (req, res) => {
+const result = pool.query('SELECT * FROM produtos')
+res.send(result.rows)
+
+});
+
+app.post('/produtos', (req, res) => {
+    const result = pool.query('SELECT * FROM produtos')
+    res.send(result.rows)
+});
+
+app.put('/produtos', (req, res) => {
+const result = pool.query('SELECT * FROM produtos')
+res.send(result.rows)
+});
+
+app.delete('/produtos', (req, res) => {
+const result = pool.query('SELECT * FROM produtos')
+res.send(result.rows)
+});
+
+app.listen(5000) 
